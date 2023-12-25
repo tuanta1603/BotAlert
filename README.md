e8bb8da2d8eb63f6d0d4ae70725be8ef6e567d74
tuanta1603 <anhtuan95.it@gmail.com>
2023-12-25 21:33:36 +0700

    tuanta_Æwebhook

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 run:  uvicorn main:app --reload
+docs: http://127.0.0.1:8000/docs
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from schemas.bot_alert import BotAlert
 from config.db import con
 from models.index import bot_alert as model_bot_alert
@@ -6,45 +6,38 @@ from models.index import bot_alert as model_bot_alert
 app = FastAPI()
 
 
-# @app.get("/")
-# async def root():
-#     return {"message": "Hello World"}
-#
-#
-# @app.get("/hello/{name}")
-# async def say_hello(name: str):
-#     return {"message": f"Hello {name}"}
-
-
 @app.post('/api/bot_alert')
 async def create_bot_alert(botalert: BotAlert):
-    data = con.execute(model_bot_alert.insert().values(
-        syminfo=botalert.syminfo,
-        prefix=botalert.prefix,
-        ticker=botalert.ticker,
-        basecurrency=botalert.basecurrency,
-        currency=botalert.currency,
-        timeframe=botalert.timeframe,
-        side=botalert.side,
-        position=botalert.position,
-        Action=botalert.Action,
-        prices_pos=botalert.prices_pos,
-        prices_tp=botalert.prices_tp,
-        precent_tp=botalert.precent_tp,
-        pos_leverage=botalert.pos_leverage,
-        pos_currency=botalert.pos_currency,
-        pos_quantity=botalert.pos_quantity,
-        CurrencyManual=botalert.CurrencyManual,
-        QuantityManual=botalert.QuantityManual,
-        codename=botalert.codename,
-        type=botalert.type
-    ))
-    con.commit()
-    if data.is_insert:
-        return {
-            'status': 'success',
-        }
-    else:
-        return {
-            'status': 'fail',
-        }
+    try:
+        # Insert data into the database
+        data = con.execute(model_bot_alert.insert().values(
+            syminfo=botalert.syminfo,
+            prefix=botalert.prefix,
+            ticker=botalert.ticker,
+            basecurrency=botalert.basecurrency,
+            currency=botalert.currency,
+            timeframe=botalert.timeframe,
+            side=botalert.side,
+            position=botalert.position,
+            Action=botalert.Action,
+            prices_pos=botalert.prices_pos,
+            prices_tp=botalert.prices_tp,
+            precent_tp=botalert.precent_tp,
+            pos_leverage=botalert.pos_leverage,
+            pos_currency=botalert.pos_currency,
+            pos_quantity=botalert.pos_quantity,
+            CurrencyManual=botalert.CurrencyManual,
+            QuantityManual=botalert.QuantityManual,
+            codename=botalert.codename,
+            type=botalert.type
+        ))
+        con.commit()
+
+        # Check if the data was inserted successfully
+        if data.is_insert:
+            return {'status': 'success'}
+        else:
+            raise HTTPException(status_code=500, detail='Failed to insert data into the database')
+    except Exception as e:
+        # Handle exceptions and provide appropriate error response
+        return {'status': 'fail', 'error_message': str(e)}
