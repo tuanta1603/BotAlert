febd95a6722dff1d0cb8fd9f202bc63b2acf4992
Sonpx <sonpxvn@gmail.com>
2023-12-31 16:32:26 +0000

    update db

--- config/db.py
+++ config/db.py
@@ -1,4 +1,4 @@
 from sqlalchemy import create_engine, MetaData
-engine = create_engine('mysql+pymysql://root:123456@localhost:3306/trading_api')
+engine = create_engine('mysql+pymysql://bingx:Hanoi!23Vietnam@localhost:3306/bingx')
 meta = MetaData()
 con = engine.connect()
