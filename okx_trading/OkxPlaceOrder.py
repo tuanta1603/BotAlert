0df6210e33977a66ae1106dbda50789319028fb3
tuanta1603 <anhtuan95.it@gmail.com>
2024-01-01 01:45:35 +0700

    tuanta-okx

--- okx_trading/OkxPlaceOrder.py
+++ okx_trading/OkxPlaceOrder.py
@@ -2,30 +2,31 @@ import okx.Trade as Trade
 
 
 def place_order(botalert, apikey, secretkey, passphrase, total_balance, min_slot_play):
-    flag = "1"  # Production trading: 0, Demo trading: 1
+    flag = "0"  # Production trading: 0, Demo trading: 1
     trade_api = Trade.TradeAPI(apikey, secretkey, passphrase, False, flag)
+    # Modify the 'side' parameter based on botalert.side
+    if botalert.position == "Long":
+        side_param = "buy"
+    elif botalert.position == "Short":
+        side_param = "sell"
+    else:
+        # Handle the case where botalert.side is neither "Long" nor "Short"
+        side_param = ""  # Set a default value or raise an error, depending on your needs
+    # Calculate 1% of total_balance
+    sz = total_balance * 0.01
 
     # Simple mode, limit order
     result = trade_api.place_order(
-        # Demo trading
-        instId="BTC-USDT",
-        tdMode="spot_isolated",
-        clOrdId="b15",
-        side="buy", # lấy từ Position , nếu position là Long thì => Buy, còn Short là sell
-        ordType="limit",
-        px="2.15",
-        ordType="market",
-        posSide="", # lấy từ position = botalert['position']
-        sz="min_slot_play",  # 1% của tổng balance hiện có, nên phải get được current balance hiện có.
-        tpOrdPx=botalert['prices_tp'], 
         # Production
-        # instId=botalert.ticker,
-        # tdMode="cash",  # Update with the appropriate value from botalert or use a default
-        # clOrdId=botalert.codename,  # Use a relevant field from botalert
-        # side=botalert.side,
-        # ordType="limit",  # Update with the appropriate value from botalert or use a default
-        # px=botalert.prices_pos,
-        # sz=botalert.pos_quantity
+        instId=botalert.ticker,
+        tdMode="spot_isolated",  # Update with the appropriate value from botalert or use a default
+        clOrdId=botalert.codename,  # Use a relevant field from botalert
+        side=side_param,
+        ordType="market",  # Update with the appropriate value from botalert or use a default
+        posSide=botalert.position,
+        px=botalert.prices_pos,
+        sz=sz,
+        tpOrdPx=botalert.prices_tp
     )
     print("result", result)
     return result
